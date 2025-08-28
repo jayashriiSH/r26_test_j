8a01b185d8c0381824170ddd753885cd2b04e243
jayashriiSH <pcmj.jayashriishankar@gmail.com>
2025-08-28 22:43:26 +0530

    5

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -25,6 +25,33 @@ double Odometry::angle(int x1, int y1, int x2, int y2) {
   // atan2 returns radians, convert to degrees
   return atan2(y2 - y1, x2 - x1) * 180.0 / M_PI;
 }
+#include "odometry.h"
+#include <ctime>
+#include <iterator>
+#include <numeric>
+#include <cmath>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+using namespace std;
+
+Odometry::Odometry(double wheel_radius, double rpm)
+    : radius(wheel_radius), rpm(rpm) {
+  // Linear velocity (m/s) =(wheel circumference * revolutions per second)
+  double rps = rpm / 60.0;
+  linear_vel = 2 * M_PI * radius * rps;
+}
+
+double Odometry::distance(int x1, int y1, int x2, int y2) {
+  return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+double Odometry::angle(int x1, int y1, int x2, int y2) {
+  // atan2 returns radians, convert to degrees
+  return atan2(y2 - y1, x2 - x1) * 180.0 / M_PI;
+}
 MotionCommand Odometry::computeCommands(vector<pair<int,int>> &path) {
     MotionCommand res;
     res.time_sec = 0.0;
@@ -46,3 +73,4 @@ MotionCommand Odometry::computeCommands(vector<pair<int,int>> &path) {
 
     return res;
 }
+
